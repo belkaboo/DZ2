5aabf11a4cc8dd00ddc2e03054a4a0e74bccaddc
belka.boo <ibelka.boo@gmail.com>
2022-10-23 00:47:50 +0300

    task13compile

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿bool Weekend(int number)
+{
+    return number == 6 || number == 7;
+}
+
+
+Console.WriteLine("Input number of the  day: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (Weekend(dayNumber))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
