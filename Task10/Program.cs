656b1ba72e389ab2267243cf86bb82a05eabde82
belka.boo <ibelka.boo@gmail.com>
2022-10-22 18:55:54 +0300

    Task10compile

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Task10
-Console.Clear();
 
+Console.Clear();
+Console.WriteLine("Input three-digit number ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 100 | number > 999)
+{
+    Console.WriteLine("It's not a three-digit number. Try again!");
+    Console.Write("Input three-digit number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int secondDigit = (number / 10) % 10;
+Console.WriteLine($"Second digit = {secondDigit}");
