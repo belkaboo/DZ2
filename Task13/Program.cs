078ca81e57c17925da64831d3556f778a628308f
belka.boo <ibelka.boo@gmail.com>
2022-10-23 00:13:00 +0300

    task13compile

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,8 +1,32 @@
-﻿// Task10
-/*
-1. определить любое число 2. создать массив, наполнить цифрами числа.( ?? 
-как определить кол-во цифр?? попробовать while % и count++. count кол-во цифр)
-2. вывод - array[2]
-3. проверка на ошибки если count <3. (ну или условие при вводе)
-*/
+﻿Console.Clear();
+Console.WriteLine("Input Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 100)
+{
+    Console.WriteLine("Third digit does't exist. Try again: ");
+    Console.WriteLine("Input Number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int digitLength = number;
+int digits = 0;
+
+while (digitLength > 0)
+{
+    digitLength = digitLength / 10;
+    digits++;
+}
+
+int[] array = new int[digits];
+
+for (int i = 0; i < array.Length; i++)
+{
+    int lastValue = number % 10;
+    array[i] = lastValue;
+    number = number / 10;
+}
+int thirdDigit = array[digits-3];
+Console.WriteLine($"Third digit = {thirdDigit}");
+
 
